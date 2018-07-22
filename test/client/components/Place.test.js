0f3c38dc8602a7f9ff11d6c5ca2de21dbdea381f
Lorence Lim <jlorencelim@gmail.com>
2018-07-22 12:35:11 +0800

    Added unit tests for the detailed data on the Place component

--- test/client/components/Place.test.js
+++ test/client/components/Place.test.js
@@ -19,3 +19,39 @@ test('With Jest snapshot, Place component renders rating section when present', 
     .toJSON();
   expect(placeComponent).toMatchSnapshot();
 });
+
+test('With Enzyme, Place component not renders carousel when place has no photos', () => {
+  const place = { hehe: 'haha', photos: [] };
+  const wrapper = shallow(
+    <Place place={place} detailedData />,
+  );
+  const p = wrapper.find('.carousel');
+  expect(p.length).toBe(0);
+});
+
+test('With Enzyme, Place component renders carousel when place has photos', () => {
+  const place = { hehe: 'haha', photos: ['photo.png'] };
+  const wrapper = shallow(
+    <Place place={place} detailedData />,
+  );
+  const p = wrapper.find('.carousel');
+  expect(p.length).toBe(1);
+});
+
+test('With Enzyme, Place component renders detail link when not detailed', () => {
+  const place = { hehe: 'haha', id: 1 };
+  const wrapper = shallow(
+    <Place place={place} detailedData={false} />,
+  );
+  const p = wrapper.find('.link');
+  expect(p.length).toBe(1);
+});
+
+test('With Enzyme, Place component not renders link when datailed', () => {
+  const place = { hehe: 'haha', id: 1 };
+  const wrapper = shallow(
+    <Place place={place} detailedData />,
+  );
+  const p = wrapper.find('.link');
+  expect(p.length).toBe(0);
+});
